5fec4eb5c3758b5a67b9588f11daab6fd9ad15f6
Thiago <programadorthiagolopes@gmail.com>
2022-06-22 10:21:19 -0300

    Revisado os exercicios aula 2

---
+++ fundamentos/bloco-4-intrudocao-a-javascript/bloco-4-array-loop-for/exerciciosresultados.js
@@ -0,0 +1,91 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for(let i = 0; i < numbers.length; i += 1){
+    console.log(numbers[i]);
+}
+*/
+
+//Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for(let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+    
+}
+console.log(soma);
+*/
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+for(let i = 0; i < numbers.length; i += 1){
+    media += numbers[i] / numbers.length;
+}
+console.log(media.toFixed(2))
+*/
+
+// /Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+for(let i = 0; i < numbers.length; i += 1){
+    media += numbers[i] / numbers.length;
+}
+console.log(media.toFixed(2));
+
+if(media > 20){
+    console.log("Valor maior que 20");
+}else{
+    console.log("Valor menor que 20");
+}
+*/
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = numbers[0];
+for(let i = 1; i < numbers.length; i+= 1){
+    if(numbers[i] > maior){
+        maior = numbers[i];
+    }
+}
+console.log("O maior valor é: " + maior)
+*/
+// /Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let next=[];
+for(let i = 0; i < numbers.length; i += 1){
+    if(numbers[i] % 2 !== 0){
+        next.push(numbers[i]);
+
+    }
+}
+console.log(next)*/
+
+//Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = numbers[0];
+
+for(let i = 0; i < numbers.length; i+=1){
+    if(numbers[i] < menor){
+        menor = numbers[i];
+    }
+}
+console.log(menor);*/
+
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+/*let array = [];
+for(let i = 1; i <= 25; i+=1){
+    array.push(i);
+    array
+}
+console.log(array);*/
+
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+/*let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+for(let i = 0; i < array.length; i+= 1){
+    array[i] = array[i] / 2;
+}
+console.log(array);*/
