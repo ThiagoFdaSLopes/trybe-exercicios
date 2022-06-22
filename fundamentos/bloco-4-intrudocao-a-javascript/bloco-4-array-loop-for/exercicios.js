0c725bca0c500da64c358e3657323d1cabd1031c
Thiago <programadorthiagolopes@gmail.com>
2022-06-22 09:15:14 -0300

    Add novos exercicios

--- fundamentos/bloco-4-intrudocao-a-javascript/bloco-4-array-loop-for/exercicios.js
+++ fundamentos/bloco-4-intrudocao-a-javascript/bloco-4-array-loop-for/exercicios.js
@@ -1,14 +1,14 @@
 //bubble sort
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let next = [];
 
-for(let i = 1; i < numbers.length; i+= 1){
-    for(let iDois = 0; iDois < i; iDois+= 1){
-        if(numbers[i] > numbers[iDois]){
-            let position = numbers[i];
-            numbers[i] = numbers[iDois];
-            numbers[iDois] = position;
-        }
+for(let i = 0; i < numbers.length; i += 1){
+    if(i + 1 < numbers.lenght){
+        next.push(numbers[i] * numbers[i + 1]);
+    }
+    else{
+        next.push(numbers[i] * 2);
     }
 }
-console.log(numbers);
+// console.log(next)
