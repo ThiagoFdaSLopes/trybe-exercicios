1938f98714f09ea76edbd091c31c843d2553f2e4
Thiago <programadorthiagolopes@gmail.com>
2022-06-23 11:11:11 -0300

    Fim do teste

--- fundamentos/bloco-4-array-loop-for/mega-sena.js
+++ fundamentos/bloco-4-array-loop-for/mega-sena.js
@@ -1,20 +1,24 @@
-// let megaSena = [];
+let megaSena = [];
 
-// let escolhaMega= [];
+let escolhaMega= [];
 
-// for(let i = 0; i < 6; i += 1){
-//     megaSena.push(Math.floor(Math.random() * 60) + 1);
-//     escolhaMega.push(Math.floor(Math.random() * 60) + 1);
-// }
-// console.log(megaSena);
-// console.log(escolhaMega);
+for(let i = 0; i < 6; i += 1){
+    megaSena.push(Math.floor(Math.random() * 60) + 1);
+    escolhaMega.push(Math.floor(Math.random() * 60) + 1);
+    if(teste){
+        megaSena.pop();
+        escolhaMega.pop();
+    }
+}
+console.log(megaSena);
+console.log(escolhaMega);
 
-// let count = 0;
-// for(let i = 0; i < megaSena.length; i += 1){
-//     for(let iDois = 0; iDois < escolhaMega.lenght; iDois += 1){
-//         if(megaSena[i] === megaSena[iDois]){
-//             count += 1;
-//         }
-//     }
-// }
-// console.log(count);
+let count = 0;
+for(let i = 0; i < megaSena.length; i += 1){
+    for(let iDois = 0; iDois < escolhaMega.lenght; iDois += 1){
+        if(megaSena[i] === megaSena[iDois]){
+            count += 1;
+        }
+    }
+}
+console.log(count);
