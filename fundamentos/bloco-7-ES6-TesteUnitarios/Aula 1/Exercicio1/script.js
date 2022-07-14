50d407395da1d49d9797b78378be6be1855044ca
Thiago <programadorthiagolopes@gmail.com>
2022-07-14 19:16:48 -0300

    Dia 1 do bloco 7

---
+++ fundamentos/bloco-7-ES6-TesteUnitarios/Aula 1/Exercicio1/script.js
@@ -0,0 +1,19 @@
+// const testingScope = (escopo) => {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope}  ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   testingScope(false);
+
+// Seu código aqui.
+// const ordem = (a, b) => {return a - b}
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+
+// console.log(oddsAndEvens.sort(ordem)); // será necessário alterar essa linha 😉
