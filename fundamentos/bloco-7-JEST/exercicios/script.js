c04c23c7b5d9c78170c835d80c080220b957c533
Thiago <programadorthiagolopes@gmail.com>
2022-07-18 21:22:23 -0300

    Exercicio do dia 1 e 2 Completed

---
+++ fundamentos/bloco-7-JEST/exercicios/script.js
@@ -0,0 +1,35 @@
+// function sum(a, b) {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         throw new Error('parameters must be numbers');
+//     }
+
+//     return a + b;
+// }
+
+// module.exports = {
+//     sum,
+// }
+
+//exercicio2
+// function myRemove(arr, item) {
+//     let newArr = [];
+//     for (let index = 0; index < arr.length; index += 1) {
+//         if (item !== arr[index]) {
+//             newArr.push(arr[index]);
+//         }
+//     }
+//     return newArr;
+// }
+
+// module.exports = myRemove;
+
+// Exercicio 3
+function myFizzBuzz(num) {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+}
+
+module.exports = myFizzBuzz;
---
+++ fundamentos/bloco-7-JEST/exercicios/script.test.js
@@ -0,0 +1,29 @@
+const myFizzBuzz = require('./script');
+
+// describe('Testando a função SOMA: ', () => {
+
+//     it('Teste se o retorno de sum(4, 5) é 9 e sum(0,0) é igual a 0', ()=>{
+//         expect(sum(4,5)).toBe(9);
+//         expect(sum(0,0)).toBe(0);
+//     });
+//     it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)', ()=> {
+//         expect(() => sum(4, '5')).toThrow();
+//         expect(() => sum(4, '5')).toThrowError(('parameters must be numbers'));
+//     });
+// });
+
+// EXERCICIO 2
+
+// describe('Criando teste para a função REMOVE', () => {
+//     it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+//         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+//     });
+//     it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+//         expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+//     });
+//     it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+//         expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+//     })
+// });
+
+// Exercicio 3
