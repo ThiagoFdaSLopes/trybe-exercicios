c0feea524bf73d81d055c1119c118485a308ecb4
Thiago <programadorthiagolopes@gmail.com>
2022-07-21 21:43:56 -0300

    Atualizado para nova forma

--- fundamentos/bloco-8-introducao-hofs/vamospraticar-01/script.js
+++ fundamentos/bloco-8-introducao-hofs/vamospraticar-01/script.js
@@ -7,3 +7,9 @@ const doingThings = (func) => console.log(func());
 doingThings(acorda);
 doingThings(coffe);
 doingThings(sleep);
+
+// // Ao chamar a função doingThings:
+// doingThings(acorda);
+
+// // Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// // Acordando!!
