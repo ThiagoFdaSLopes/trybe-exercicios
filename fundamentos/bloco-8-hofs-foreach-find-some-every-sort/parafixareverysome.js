bf116bad5131b96f441234873be349eee8dab8d1
Thiago <programadorthiagolopes@gmail.com>
2022-07-22 15:16:21 -0300

    Feito o exercicio do every

--- fundamentos/bloco-8-hofs-foreach-find-some-every-sort/parafixareverysome.js
+++ fundamentos/bloco-8-hofs-foreach-find-some-every-sort/parafixareverysome.js
@@ -9,17 +9,15 @@
 
 const people = [
     { name: 'Mateus', age: 18 },
-    { name: 'José', age: 18 },
+    { name: 'José', age: 16 },
     { name: 'Ana', age: 23 },
     { name: 'Cláudia', age: 20 },
     { name: 'Bruna', age: 19 },
 ];
 
-console.log(Object.values(people[0]))
+const verifyAges = (arr, minimumAge) => {
+    //Adicione seu código aqui
+    return arr.every((element) => element.age >= minimumAge);
+}
 
-// const verifyAges = (arr, minimumAge) => {
-//     //Adicione seu código aqui
-//     return Object.values(arr.age).every((element) => element >= minimumAge);
-// }
-
-// console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 18));
