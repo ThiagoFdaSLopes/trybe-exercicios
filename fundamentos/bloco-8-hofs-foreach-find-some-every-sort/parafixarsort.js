7b4c95ea7e8507779d0a006b7deb831647f51722
Thiago <programadorthiagolopes@gmail.com>
2022-07-23 08:34:43 -0300

    Iniciando estudos sabado

--- fundamentos/bloco-8-hofs-foreach-find-some-every-sort/parafixarsort.js
+++ fundamentos/bloco-8-hofs-foreach-find-some-every-sort/parafixarsort.js
@@ -1,18 +1,18 @@
-const people = [
-    { name: 'Mateus', age: 18 },
-    { name: 'José', age: 16 },
-    { name: 'Ana', age: 23 },
-    { name: 'Cláudia', age: 20 },
-    { name: 'Bruna', age: 19 },
-];
+// const people = [
+//     { name: 'Mateus', age: 18 },
+//     { name: 'José', age: 16 },
+//     { name: 'Ana', age: 23 },
+//     { name: 'Cláudia', age: 20 },
+//     { name: 'Bruna', age: 19 },
+// ];
 
-// Adicione se código aqui
+// // Adicione se código aqui
+// // people.sort((a, b) => {
+// //     return a.age - b.age
+// // });
 // people.sort((a, b) => {
-//     return a.age - b.age
+//     return b.age - a.age
 // });
-people.sort((a, b) => {
-    return b.age - a.age
-});
 
 
-console.log(people);
+// console.log(people);
