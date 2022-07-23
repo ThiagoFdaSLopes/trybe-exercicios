9ebba815e0f9a3e222c9de44bc8874b1763f3710
Thiago <programadorthiagolopes@gmail.com>
2022-07-23 08:38:46 -0300

    Exercicio 1 resolvido

--- fundamentos/bloco-8-hofs-foreach-find-some-every-sort/exercicios/exercise1.js
+++ fundamentos/bloco-8-hofs-foreach-find-some-every-sort/exercicios/exercise1.js
@@ -1,12 +1,68 @@
-const people = [
-    { name: 'Mateus', age: 18 },
-    { name: 'José', age: 16 },
-    { name: 'Ana', age: 23 },
-    { name: 'Cláudia', age: 20 },
-    { name: 'Bruna', age: 19 },
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
 ];
-  
-// Adicione se código aqui
-people.sort((c, b) => c.age - b.age);
 
-console.log(people);
+  // Adicione o código do exercício aqui:
+  // 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+  const authorBornIn1947 = (array, ano) => array.find((element) => element.author.birthYear === ano).name
+
+  console.log(authorBornIn1947(books, 1947));
