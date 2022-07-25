760e6d1b42571e462c4c36a9e90f2723ae1852d0
Thiago <programadorthiagolopes@gmail.com>
2022-07-25 16:02:35 -0300

    Exercicio 4 resolvido

---
+++ fundamentos/bloco-8-map-filter/exercicios/exercise4.js
@@ -0,0 +1,80 @@
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
+];
+
+  // Adicione o código do exercício aqui:
+
+/*
+🚀 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+Dica: use as funções filter e sort
+*/
+
+const oldBooksOrdered = (arr, ano) => {
+    return arr.filter((element) => {
+        const date = ano;
+        if(date - element.releaseYear > 60) {
+            return element
+        }
+    }).sort((a, b)=> (a.releaseYear -2022) - (b.releaseYear - 2022))
+}
+
+console.log(oldBooksOrdered(books, 2022))
