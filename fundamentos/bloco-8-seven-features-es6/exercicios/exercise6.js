20657e5742bdd67ab54b9fb4eb1c29159272ff82
Thiago <programadorthiagolopes@gmail.com>
2022-07-27 16:05:41 -0300

    refeito exercicio 6

--- fundamentos/bloco-8-seven-features-es6/exercicios/exercise6.js
+++ fundamentos/bloco-8-seven-features-es6/exercicios/exercise6.js
@@ -11,12 +11,6 @@ const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
 // escreva toObject abaixo
-const toObject = ([nome, marca, ano]) => {
-    return {
-        nome,
-        marca,
-        ano,
-    }
-}
+const toObject = ([nome, marca, ano]) => ({nome, marca, ano})
 
 console.log(toObject(palio))
