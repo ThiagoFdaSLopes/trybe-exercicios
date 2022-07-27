253ef8f7df560675f10045078684e40c60594266
Thiago <programadorthiagolopes@gmail.com>
2022-07-27 14:22:38 -0300

    Feito para fixar array destructuring

---
+++ fundamentos/bloco-8-seven-features-es6/parafixararraysdestructuring.js
@@ -0,0 +1,33 @@
+// const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+// const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+// console.log(firstCountry); // Brazil
+// console.log(secondCountry); // Japan
+// console.log(thirdCountry); // China
+// console.log(fourthCountry); // Canada
+
+// 1 - Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+// const [ saudacao, funcaosaudacao ] = saudacoes;
+
+// funcaosaudacao(saudacao);
+
+// 2 - A seguir, temos alguns valores que estão descritos em variáveis incoerentes. 
+//Através da desestruturação de arrays, corrija os valores das variáveis.
+const nomes = ['gato', 'água', 'arroz']
+// let comida = 'gato';
+// let animal = 'água';
+// let bebida = 'arroz';
+const [ animal, bebida, comida ] = nomes
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+//3 - array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. 
+//Através de array destructuring, faça com que existam apenas números pares na variável numerosPares.
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+const [,,, ...numeros] = numerosPares;
+
+console.log(numeros)
