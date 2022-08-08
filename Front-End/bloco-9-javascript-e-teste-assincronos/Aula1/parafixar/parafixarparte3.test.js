1e2e5b3a8367f8c544c6c7610a069c87e401d482
Thiago <programadorthiagolopes@gmail.com>
2022-08-08 18:37:27 -0300

    Testando os testes

--- Front-End/bloco-9-javascript-e-teste-assincronos/Aula1/parafixar/parafixarparte3.test.js
+++ Front-End/bloco-9-javascript-e-teste-assincronos/Aula1/parafixar/parafixarparte3.test.js
@@ -1,4 +1,4 @@
-const asyncSum = require('./parafixarparte3')
+// const asyncSum = require('./parafixarparte3')
 
 // test('Não deveria passar!', () => {
 //     setTimeout(() => {
@@ -7,26 +7,61 @@ const asyncSum = require('./parafixarparte3')
 //     }, 500);
 // });
 
-test('Não deveria passar!', (done) => {
+// test('Não deveria passar!', (done) => {
     
-    setTimeout(() => {
-    try {
-      expect(10).toBe(5);
-      console.log('Deveria falhar!');
-      done();
-    } catch (error) {
-        done(error);
-    }
-    }, 500);
-});
+//     setTimeout(() => {
+//     try {
+//       expect(10).toBe(5);
+//       console.log('Deveria falhar!');
+//       done();
+//     } catch (error) {
+//         done(error);
+//     }
+//     }, 500);
+// });
 
-test('Testando asyncSum, soma 5 mais 10', (done) => {
-    asyncSum(5, 10, (result) => {
-      try {
-        expect(result).toBe(15);
-        done();
-      } catch (error) {
-        done(error);
-      }
-    });
+// test('Testando asyncSum, soma 5 mais 10', (done) => {
+//     asyncSum(5, 10, (result) => {
+//       try {
+//         expect(result).toBe(15);
+//         done();
+//       } catch (error) {
+//         done(error);
+//       }
+//     });
+// });
+
+// test('verify matcher toBe', () => {
+//   const food = 'Coxinha';
+//   expect(food).toBe('Coxinha');
+// });
+
+// test('verify matcher toBe', () => {
+//   expect(10).toBe('10');
+// });
+// test('verify matcher toEqual', () => {
+//   const name = 'Anna'
+//   expect(name).toEqual(['Anna']);
+// });
+// test('verify matcher toEqual', () => {
+//   expect(5).toEqual('5');
+// });
+// test('verify matcher toEqual', () => {
+//   const character = { name: 'Alice' }
+//   const movie = { name: 'Alice in Wonderland' }
+
+//   expect(character).toEqual(movie);
+// });
+// test('verify matcher toEqual', () => {
+//   const animal = { type: 'Cat' }
+//   const animalName = 'Cat';
+
+//   expect(animal).toEqual(animalName);
+// });
+
+test('verify matcher toEqual', () => {
+  const character = { name: 'Ariel' };
+  const person = { name: 'Ariel' };
+
+  expect(character).toEqual(person);
 });
