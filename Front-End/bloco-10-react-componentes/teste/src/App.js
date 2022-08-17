8531462f6929598b7656c8cee1cde004caf3927b
Thiago <programadorthiagolopes@gmail.com>
2022-08-17 09:53:05 -0300

    Feito para fixar de Props

--- Front-End/bloco-10-react-componentes/teste/src/App.js
+++ Front-End/bloco-10-react-componentes/teste/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import Teste from './Teste';
+// import Teste from './Teste';
+import Image from './Img';
 
 function App() {
   return (
     <div className="App">
-      <Teste name='Thiago' lastName='Lopes'/>
+      {/* <Teste name='Thiago' lastName='Lopes'/> */}
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
     </div>
   );
 }
