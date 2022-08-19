e34f089d2c59b17cd6b0a2475645edf1da0955c8
Thiago <programadorthiagolopes@gmail.com>
2022-08-19 11:46:33 -0300

    Feito para fixar final

--- Front-End/bloco-11-componentes-com-estado-eventos-e-forms/componentes-e-eventos/aula-1/fancy-buttons/src/App.js
+++ Front-End/bloco-11-componentes-com-estado-eventos-e-forms/componentes-e-eventos/aula-1/fancy-buttons/src/App.js
@@ -9,6 +9,13 @@ class App extends Component {
     this.setState((antes, _props) => ({
       numeroCliques: antes.numeroCliques + 1
     }));
+    const { numeroCliques } = this.state;
+
+    if(numeroCliques % 2 === 0) {
+      console.log(numeroCliques)
+      console.log('PAR')
+      console.log(this)
+    }
   }
   render() {
     const { numeroCliques } = this.state;
