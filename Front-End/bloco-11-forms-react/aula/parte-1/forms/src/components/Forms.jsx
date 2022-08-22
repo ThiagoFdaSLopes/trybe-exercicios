a1c5a147e6ce19c72b100716d1a80ccefdc8198d
Thiago <programadorthiagolopes@gmail.com>
2022-08-22 16:15:59 -0300

    Feito parte 2 aula 1

--- Front-End/bloco-11-forms-react/aula/parte-1/forms/src/components/Forms.jsx
+++ Front-End/bloco-11-forms-react/aula/parte-1/forms/src/components/Forms.jsx
@@ -4,25 +4,41 @@ class Forms extends Component {
     constructor(){
         super()
 
-        this.state = {
-            name: '',
-        }
+        this.state = {}
+
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange = (event) => {
+    handleChange = ({ target }) => {
+        const { name } = target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         this.setState(() => ({
-            name: event.target.value
+            [name]: value,
         }))
+        console.log(this.state.value);
     }
 
     render() {
         return (
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
-                </label>
-            </form>
+          <form>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Quais comidas você gosta?
+              <select name="suco" onChange={this.handleChange}>
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+                <option value="coconut">Coconut</option>
+                <option value="mango">Mango</option>
+              </select>
+            </label>
+          </form>
         );
     }
 }
