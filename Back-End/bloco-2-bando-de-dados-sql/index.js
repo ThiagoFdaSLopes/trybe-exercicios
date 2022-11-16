d979d9bdbb93f2c2008ace16f6aedf3aa53ef5f6
Thiago <programadorthiagolopes@gmail.com>
2022-11-16 11:19:16 -0300

    feat: Flags usadas

--- Back-End/bloco-2-bando-de-dados-sql/index.js
+++ Back-End/bloco-2-bando-de-dados-sql/index.js
@@ -2,3 +2,13 @@
 
 // Comando para gerar imagem docker com mysql
 // docker container run --name container-mysql -e MYSQL_ROOT_PASSWORD=senha-mysql -d -p 3306:3306 mysql
+
+// flagas usadas:
+
+// –name: Através desta flag, estamos nomeando o nosso container e facilitando a identificação do mesmo;
+
+// -e: Utilizada para informar nossas variáveis de ambiente. No nosso exemplo, como iremos acessar o container como usuário root, passaremos a seguinte variável de ambiente e senha MYSQL_ROOT_PASSWORD=senha-mysql;
+
+// -d: Utilizada para fazer com que o nosso container rode em segundo plano;
+
+// -p: Indicaremos qual porta no nosso sistema operacional o docker estará em funcionamento. Perceba que passamos 3306:3306. A porta 3306 (lado esquerdo) significa que a porta 3306 do nosso computador receberá o container docker. A porta 3306 (lado direito) significa que a porta que teremos acesso dentro do container. Lembrando que 3306 é a porta padrão do MySQL.
