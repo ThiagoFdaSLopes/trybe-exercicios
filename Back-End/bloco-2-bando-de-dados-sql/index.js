bc99278abf733158ee7cfb4b9320a17ce709e3cb
Thiago <programadorthiagolopes@gmail.com>
2022-11-16 11:18:41 -0300

    feat: Criado comando para gerar docker container com imagem do mysql

--- Back-End/bloco-2-bando-de-dados-sql/index.js
+++ Back-End/bloco-2-bando-de-dados-sql/index.js
@@ -1,1 +1,4 @@
 // Iniciando dia do banco de dados
+
+// Comando para gerar imagem docker com mysql
+// docker container run --name container-mysql -e MYSQL_ROOT_PASSWORD=senha-mysql -d -p 3306:3306 mysql
