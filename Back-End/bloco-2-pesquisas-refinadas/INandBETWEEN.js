800f3f76a2d633f16788b804794affb7c78606b4
Thiago <programadorthiagolopes@gmail.com>
2022-11-18 10:47:00 -0300

    docs: IN and BETWEEN

---
+++ Back-End/bloco-2-pesquisas-refinadas/INandBETWEEN.js
@@ -0,0 +1,17 @@
+// USE sakila;
+
+// SELECT * FROM payment 
+// WHERE customer_id IN (269, 239, 126, 399, 142);
+
+// SELECT * FROM address
+// WHERE district IN ('QLD', 'Nagasaki', 'California', 'Attika', 'andalay', 'Nantou', 'Texas');
+
+// SELECT * FROM payment
+// WHERE amount BETWEEN 0.99 AND 1.99;
+
+// SELECT * FROM language
+// WHERE name BETWEEN 'Italian' AND 'Mandarin'
+// ORDER BY name;
+
+// SELECT rental_id, rental_date FROM rental
+// WHERE rental_date BETWEEN '2005-05-27' AND '2005-07-17';
