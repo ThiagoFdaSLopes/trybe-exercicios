293ecda44913512b340eb9cae44885ac3dca1e37
Thiago <programadorthiagolopes@gmail.com>
2022-11-23 08:05:25 -0300

    feat: Feito exercicios da parte de condicionais

---
+++ Back-End/bloco-3-funcoesSQL-JOINs-Normalizacao/condicionais.sql
@@ -0,0 +1,61 @@
+-- Sintaxe:
+SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
+
+SELECT IF(idade >= 18, 'Maior de idade', 'Menor de Idade')
+FROM pessoas;
+
+SELECT IF(aberto, 'Entrada permitida', 'Entrada não permitida')
+FROM estabelecimentos;
+
+-- Exemplo utilizando o banco sakila:
+SELECT first_name, IF(active, 'Cliente Ativo', 'Cliente Inativo') AS status
+FROM sakila.customer
+LIMIT 20;
+
+-- Sintaxe:
+SELECT CASE
+  WHEN condicao THEN valor
+  ELSE valor padrao
+END;
+
+SELECT
+    nome,
+    nivel_acesso,
+    CASE
+        WHEN nivel_acesso = 1 THEN 'Nível de acesso 1'
+        WHEN nivel_acesso = 2 THEN 'Nível de acesso 2'
+        WHEN nivel_acesso = 3 THEN 'Nível de acesso 3'
+        ELSE 'Usuário sem acesso'
+    END AS nivel_acesso
+FROM permissoes_usuario;
+
+-- Exemplo utilizando a tabela sakila.film:
+SELECT
+    first_name,
+    email,
+    CASE
+        WHEN email = 'MARY.SMITH@sakilacustomer.org' THEN 'Cliente de baixo valor'
+        WHEN email = 'PATRICIA.JOHNSON@sakilacustomer.org' THEN 'Cliente de médio valor'
+        WHEN email = 'LINDA.WILLIAMS@sakilacustomer.org' THEN 'Cliente de alto valor'
+        ELSE 'não classificado'
+    END AS valor
+FROM sakila.customer
+LIMIT 10;
+
+
+-- Exercicios da pagina
+-- USANDO IF 
+SELECT film_id AS id, title AS titulo, IF(title = 'ACE GOLDFINGER', 'Já assisti esse filme', 'Não conheço esse filme') AS 'Conheço o filme'
+FROM film;
+
+
+-- USANDO CASE
+SELECT title, rating, 
+	CASE 
+		WHEN rating = 'G' THEN 'Livre para todos'
+        WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+        WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+        WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+	ELSE 'Proibido para menores de idade'
+END AS 'público-alvo'
+FROM film;
