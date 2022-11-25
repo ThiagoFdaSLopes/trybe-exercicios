3a8e8e82e949b1c59bcbe374e38d8af18451d529
Thiago <programadorthiagolopes@gmail.com>
2022-11-25 18:47:20 -0300

    feat(exercise): Concluido exercicio 2

--- Back-End/bloco-3-descomplicando-joins/exercicioDoDia.sql
+++ Back-End/bloco-3-descomplicando-joins/exercicioDoDia.sql
@@ -6,3 +6,11 @@ FROM box_office AS box
 INNER JOIN movies AS mov
 ON box.movie_id = mov.id;
 
+-- Utilizando o INNER JOIN, faça uma busca que retorne o 
+-- número de vendas para cada filme que possui um número maior de vendas internacionais (international_sales) do que vendas nacionais (domestic_sales).
+
+SELECT box.movie_id, mov.title, (box.domestic_sales + box.international_sales) AS Vendas
+FROM box_office AS box
+INNER JOIN movies AS mov
+ON box.movie_id = mov.id
+WHERE box.domestic_sales < box.international_sales;
