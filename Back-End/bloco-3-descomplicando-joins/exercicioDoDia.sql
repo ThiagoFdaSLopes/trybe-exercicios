f61bb7e18708a5c3040ffc25748928a67f74b659
Thiago <programadorthiagolopes@gmail.com>
2022-11-25 18:51:32 -0300

    feat(exercise): Concluido exercicio 3

--- Back-End/bloco-3-descomplicando-joins/exercicioDoDia.sql
+++ Back-End/bloco-3-descomplicando-joins/exercicioDoDia.sql
@@ -14,3 +14,11 @@ FROM box_office AS box
 INNER JOIN movies AS mov
 ON box.movie_id = mov.id
 WHERE box.domestic_sales < box.international_sales;
+
+-- Exercício 3: Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua avaliação (rating) em ordem decrescente.
+
+SELECT box.movie_id, m.title, box.rating
+FROM box_office AS box
+INNER JOIN movies AS m
+ON box.movie_id = m.id
+ORDER BY box.rating DESC;
