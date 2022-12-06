c1a04cdc55f28118a800ef10cfb5d9d492fdef09
Thiago <programadorthiagolopes@gmail.com>
2022-12-06 19:27:54 -0300

    feat(exercicios): Concluido exercicio 6E

--- Back-End/bloco-4-node-js/node-js-runtime-assicrono/exercicios/my-scripts/index.js
+++ Back-End/bloco-4-node-js/node-js-runtime-assicrono/exercicios/my-scripts/index.js
@@ -1,13 +1,31 @@
 const fs = require('fs').promises;
 
-async function newsSimpson() {
-  const data = await fs.readFile('./simpsons.json');
-  const result = JSON.parse(data);
+// async function newsSimpson() {
+//   const data = await fs.readFile('./simpsons.json');
+//   const result = JSON.parse(data);
 
-  const novos = JSON.stringify(result.filter((e) => e.id <= '4'));
+//   const novos = JSON.stringify(result.filter((e) => e.id <= '4'));
   
 
-  fs.appendFile('./simpsonFamily.json', novos);
+//   fs.writeFile('./simpsonFamily.json', novos);
+// }
+
+// newsSimpson();
+
+async function simpsonFamily() {
+  const data = await fs.readFile('./simpsonFamily.json');
+  const result = JSON.parse(data);
+
+  const newPerson = {
+    id: '5',
+    name: 'Nelson Muntz'
+  };
+  
+  result.push(newPerson);
+
+  const teste = JSON.stringify(result);
+
+  fs.writeFile('./simpsonFamily.json', teste);
 }
 
-newsSimpson();
+simpsonFamily();
---
+++ Back-End/bloco-4-node-js/node-js-runtime-assicrono/exercicios/my-scripts/newSimpson.js
@@ -0,0 +1,9 @@
+async function newsSimpson() {
+  const data = await fs.readFile('./simpsons.json');
+  const result = JSON.parse(data);
+
+  const novos = JSON.stringify(result.filter((e) => e.id <= '4'));
+  
+
+  fs.appendFile('./simpsonFamily.json', novos);
+}
