c586156c8593059f2f837d909df8fd13a4ff7618
Thiago <programadorthiagolopes@gmail.com>
2022-12-06 17:08:36 -0300

    fix(ajustes): Ajustes na funcao

--- Back-End/bloco-4-node-js/node-js-runtime-assicrono/exercicios/my-scripts/index.js
+++ Back-End/bloco-4-node-js/node-js-runtime-assicrono/exercicios/my-scripts/index.js
@@ -1,19 +1,13 @@
 const fs = require('fs').promises;
 
 async function readSimpsons() {
-  try {
-    const data = await fs.readFile('./simpsons.json');
-    const result = JSON.parse(data);
+  const data = await fs.readFile('./simpsons.json');
+  const result = JSON.parse(data);
 
-    for(let i = 0; i < result.length; i ++) {
-      console.log(`${result[i].id} - ${result[i].name}`);
-    }
-  } catch(error) {
-    console.log('Erro %s', error);
+  for(let i = 0; i < result.length; i ++) {
+    console.log(`${result[i].id} - ${result[i].name}`);
   }
 }
-
-async function
 
 readSimpsons();
 
