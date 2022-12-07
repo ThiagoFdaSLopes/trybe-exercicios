94f8cfefdde961c938991e036df66980ef8b41dd
Thiago <programadorthiagolopes@gmail.com>
2022-12-07 14:35:26 -0300

    feat(exercicios): Concluido exercicio 2

--- Back-End/bloco-4-api-rest-express/exercicios/exercicios.js
+++ Back-End/bloco-4-api-rest-express/exercicios/exercicios.js
@@ -4,3 +4,6 @@
 // Status Code 400 Servidor não ira processar a requisição por erro de sintaxe ou informação malformada na solicitação
 
 // Status Code 422: Servidor Não conseguiu processar as informações solicitada
+
+// Exercicio 2
+// Status Code 401: Não possui credencias de Autenticação validas para o recurso solicitado
