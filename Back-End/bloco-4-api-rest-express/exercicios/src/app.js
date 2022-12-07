47d179ada0f41ed8aa9f3a72eeaaa58d0bbbec78
Thiago <programadorthiagolopes@gmail.com>
2022-12-07 15:14:15 -0300

    feat(exercicio): Concluido exercicio 5

--- Back-End/bloco-4-api-rest-express/exercicios/src/app.js
+++ Back-End/bloco-4-api-rest-express/exercicios/src/app.js
@@ -9,4 +9,16 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).json({ json }));
 
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params;
+
+  const movie = json.find((e) => e.id === Number(id));
+
+  if (!movie) {
+    res.status(400).json({ error: 'ID não encontrado' });
+  }
+
+  res.status(200).json({ movie });
+  });
+
 module.exports = app;
