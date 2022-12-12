b3c8583a163431ef4d261e1dcb74c602c57f5d97
Thiago <programadorthiagolopes@gmail.com>
2022-12-12 18:56:10 -0300

    feat(database): Criado logica que aceita a escrita na database

--- Back-End/bloco-4-express-middlewares/exercicio/src/routes/ecoturismRoutes.js
+++ Back-End/bloco-4-express-middlewares/exercicio/src/routes/ecoturismRoutes.js
@@ -6,6 +6,7 @@ const checkDescription = require('../middlewares/checkDescription');
 const checkPrice = require('../middlewares/checkPrice');
 const checkRating = require('../middlewares/checkRating');
 const checkDifficulty = require('../middlewares/checkDifficulty');
+const writeDatabase = require('../utils/fs/writeFile');
 
 const router = express.Router();
 
@@ -18,7 +19,8 @@ router.use(checkDifficulty);
 
 router.post('/activities', 
   checkName, 
-  checkPrice, checkDescription, checkRating, checkDifficulty, checkCreatedAt, (_req, res) => {
+  checkPrice, checkDescription, checkRating, checkDifficulty, checkCreatedAt, async (req, res) => {
+  await writeDatabase(req.body);
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
