1ad3ff7d579c6a4a45b5bb905a5f97a9eddb7006
Thiago <programadorthiagolopes@gmail.com>
2022-12-12 13:29:44 -0300

    feat(routes): Criado rotas para as middlewares

--- Back-End/bloco-4-express-middlewares/apitimes/src/app.js
+++ Back-End/bloco-4-express-middlewares/apitimes/src/app.js
@@ -1,59 +1,19 @@
 const express = require('express');
-const validateTeam = require('./middlewares/validateTeam');
-const existingId = require('./middlewares/existingId');
 const apiCredentials = require('./middlewares/apiCredentials');
+const teamsRouter = require('./routes/teamsRouter');
 
 const app = express();
 app.use(apiCredentials);
-
-let nextId = 3;
-const teams = [
-  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
-  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
-];
-
 app.use(express.json());
+app.use('/teams', teamsRouter);
 
-app.get('/teams', (req, res) => res.json(teams));
-
-app.get('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  res.status(200).json(team);
+app.use((err, _req, _res, next) => {
+  console.error(err.stack);
+  next(err);
 });
 
-app.post('/teams', validateTeam, (req, res) => {
-  if (
-    // confere se a sigla proposta está inclusa nos times autorizados
-    !req.teams.teams.includes(req.body.sigla)
-    // confere se já não existe um time com essa sigla
-    && teams.every((t) => t.sigla !== req.body.sigla)
-  ) {
-    return res.sendStatus(401);
-  }
-  const team = { id: nextId, ...req.body };
-  teams.push(team);
-  nextId += 1;
-  res.status(201).json(team);
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: `Algo deu errado! Mensagem: ${err.message}` });
 });
-
-app.put('/teams/:id', existingId, validateTeam, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  const updated = { id, ...req.body };
-  teams.splice(index, 1, updated);
-  res.status(201).json(updated);
-});
-
-app.delete('/teams/:id', existingId, (req, res) => {
-  const id = Number(req.params.id);
-  const team = teams.find((t) => t.id === id);
-  const index = teams.indexOf(team);
-  teams.splice(index, 1);
-  res.sendStatus(204);
-});
-
-app.use((req, res) => res.sendStatus(404));
 
 module.exports = app;
