7ebac0a8607ea9c24809d4d58e7c274b0f66bd70
Thiago <programadorthiagolopes@gmail.com>
2022-12-13 11:01:51 -0300

    feat(mysql): Concluido cadastro de pessoa1

--- Back-End/bloco-4-express-mysql/trybecash-api/src/app.js
+++ Back-End/bloco-4-express-mysql/trybecash-api/src/app.js
@@ -4,4 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+const peopleRoutes = require('./routes/peopleRoutes');
+
+app.use('/people', peopleRoutes);
+
 module.exports = app;
--- Back-End/bloco-4-express-mysql/trybecash-api/src/routes/peopleRoutes.js
+++ Back-End/bloco-4-express-mysql/trybecash-api/src/routes/peopleRoutes.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const peopleDB = require('../db/peopleDB');
+
+router.post('/', async (req, res) => {
   const person = req.body;
-  res.status(201).json(person);
+  try {
+    const [result] = await peopleDB.insert(person);
+    res.status(201).json({
+      message: `Pessoa cadastrada com sucesso com o id ${result.insertId}`
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Ocorreu um erro ao cadastrar uma pessoa' });
+  }
 });
 
 module.exports = router;
