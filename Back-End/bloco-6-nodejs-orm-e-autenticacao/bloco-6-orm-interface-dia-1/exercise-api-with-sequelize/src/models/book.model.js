c0a58f6c8ae35bfa12bbb5cfd63de09323a99f0c
Thiago <programadorthiagolopes@gmail.com>
2023-01-20 14:30:15 -0300

    feat: Criado model para books

--- Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/models/book.model.js
+++ Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/models/book.model.js
@@ -1,11 +1,13 @@
 const BookModel = (sequelize, DataTypes) => {
-  const book = sequelize.define('Book', {
+  const Book = sequelize.define('Book', {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     pageQuantity: DataTypes.INTEGER,
-    createAt: DataTypes.DATE,
+    createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE,
   });
 
-  return book;
+  return Book;
 }
+
+module.exports = BookModel;
