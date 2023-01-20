4c11048b36e4cd805ca87ce523c11465effad3be
Thiago <programadorthiagolopes@gmail.com>
2023-01-20 14:56:50 -0300

    feat: Criado createBook no service

--- Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/services/books.service.js
+++ Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/services/books.service.js
@@ -10,10 +10,17 @@ const getById = async (idBook) => {
   if(!book) return { type: 'BOOK_NOT_FOUND', message: "Book not found" };
 
   return { type: '', message: book };
+};
+
+const createBook = async (title, author, pageQuantity) => {
+  const newBook = await Book.create({ title, author, pageQuantity });
+
+  return newBook;
 }
 
 
 module.exports = {
   getAll,
   getById,
+  createBook,
 };
