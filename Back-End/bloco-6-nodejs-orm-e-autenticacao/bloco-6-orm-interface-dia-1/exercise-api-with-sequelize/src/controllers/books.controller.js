65a162280edb589b35f0861411a60d5c8ee08d51
Thiago <programadorthiagolopes@gmail.com>
2023-01-20 14:43:40 -0300

    feat: Criado getByid no service

--- Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/controllers/books.controller.js
+++ Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/controllers/books.controller.js
@@ -4,8 +4,18 @@ const getAll = async (_req, res) => {
   const books = await BookService.getAll();
 
   res.status(200).json(books);
-}
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await BookService.getById(id);
+
+  if(type) return res.status(404).json({ message });
+
+  res.status(200).json(message);
+};
 
 module.exports = {
   getAll,
-}
+  getById,
+};
