0c2bfa02c034cb1a02b6e0ec0b8c2df45cbce100
Thiago <programadorthiagolopes@gmail.com>
2023-01-20 14:30:30 -0300

    feat: Configurado server express com routers

--- Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/app.js
+++ Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const { BooksRouter } = require('./routers');
+
 app.use(express.json());
+
+app.use('/books', BooksRouter);
 
 module.exports = app;
 
--- Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/server.js
+++ Back-End/bloco-6-nodejs-orm-e-autenticacao/bloco-6-orm-interface-dia-1/exercise-api-with-sequelize/src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
