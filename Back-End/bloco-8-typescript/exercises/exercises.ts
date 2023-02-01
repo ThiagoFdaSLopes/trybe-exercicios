d73a0fa2649c445b7b9e0b1d473d85f02360b85b
Thiago <programadorthiagolopes@gmail.com>
2023-02-01 09:45:45 -0300

    feat: Criado function para calcular area rectangle

--- Back-End/bloco-8-typescript/exercises/exercises.ts
+++ Back-End/bloco-8-typescript/exercises/exercises.ts
@@ -1,3 +1,7 @@
 export function getSquareArea(side: number): number {
   return side ** 2;
 };
+
+export function getRectangleArea(heigth: number, width: number): number {
+  return heigth * width;
+};
