441d9a5818584a2de35ddae3e9580aa75fcd2a04
Thiago <programadorthiagolopes@gmail.com>
2023-02-01 15:05:33 -0300

    feat: Completado Exercicio  1

---
+++ Back-End/bloco-8-typescript/exercicioDoDia/dist/exerciseOne.js
@@ -0,0 +1,25 @@
+"use strict";
+// ANTES
+// const numberInput = 7;
+// const isItAPrime = (param) => {
+//   for (let i = 2; i < param; i++)
+//     if (param % i === 0) {
+//       return false;
+//     }
+//   return param > 1;
+// };
+// isItAPrime(numberInput)
+//   ? console.log(`${numberInput} é primo`)
+//   : console.log(`${numberInput} não é primo`);
+// DEPOIS 
+const numberInput = 1;
+const isItAPrime = (param) => {
+    for (let i = 2; i < param; i++)
+        if (param % i === 0) {
+            return false;
+        }
+    return param > 1;
+};
+isItAPrime(numberInput)
+    ? console.log(`${numberInput} é primo`)
+    : console.log(`${numberInput} não é primo`);
