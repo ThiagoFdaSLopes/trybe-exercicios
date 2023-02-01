e1e57ee3a7766703024128c50371d9673c605299
Thiago <programadorthiagolopes@gmail.com>
2023-02-01 09:46:21 -0300

    feat: Criado function para calcular area polygon

--- Back-End/bloco-8-typescript/exercises/exercises.ts
+++ Back-End/bloco-8-typescript/exercises/exercises.ts
@@ -9,3 +9,7 @@ export function getRectangleArea(heigth: number, width: number): number {
 export function getTriangleArea(heigth: number, width: number): number {
   return (heigth * width) / 2;
 };
+
+export function getPolygonPerimeter(sides: number[]): number {
+  return sides.reduce((acc, side) => acc + side, 0);
+};
