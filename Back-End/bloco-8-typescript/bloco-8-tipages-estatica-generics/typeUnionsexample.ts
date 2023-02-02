6b9b55363e82c90225b18fe54943fcad452194e3
Thiago <programadorthiagolopes@gmail.com>
2023-02-02 09:09:44 -0300

    chore: exemplo de type unions

---
+++ Back-End/bloco-8-typescript/bloco-8-tipages-estatica-generics/typeUnionsexample.ts
@@ -0,0 +1,9 @@
+type cpf = string | number;
+
+type Vowels = "a" | "e" | "i" | "o" | "u";
+
+type SO = "linux" | "mac os" | "windows";
+
+type IdentifyingDocument = string | number;
+
+type StatesOfMatter = "liquid" | "solid" | "gaseous";
