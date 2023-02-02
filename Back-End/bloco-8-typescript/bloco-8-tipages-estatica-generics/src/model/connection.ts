a64ca5679beed5f17d762515f94c58f22a677458
Thiago <programadorthiagolopes@gmail.com>
2023-02-02 11:57:20 -0300

    feat: Criado prototipo de model com typescript

--- Back-End/bloco-8-typescript/bloco-8-tipages-estatica-generics/src/model/connection.ts
+++ Back-End/bloco-8-typescript/bloco-8-tipages-estatica-generics/src/model/connection.ts
@@ -4,8 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default mysql.createPool({
-  host: process.env.DB_HOSTNAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOSTNAME || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: 3306,
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_DATABASE || 'books_api',
 });
