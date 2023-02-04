8d72fedb52b4ef3811bb960f896dbb08c95769fa
Thiago <programadorthiagolopes@gmail.com>
2023-02-04 11:11:59 -0300

    feat: Criado metodo getById no PostController

--- Back-End/bloco-8-typescript/bloco-8-express-com-typescript/exercicios/src/controllers/post.controller.ts
+++ Back-End/bloco-8-typescript/bloco-8-express-com-typescript/exercicios/src/controllers/post.controller.ts
@@ -10,4 +10,11 @@ export default class PostController {
     if(!posts) return res.status(400).json({ message: 'Erro nao identificado'})
     res.status(200).json(posts);
   }
+
+  getById = async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const post = await this.postService.getById(id);
+    if(!post) return res.status(404).json({ message: 'Erro id nao identificado'})
+    res.status(200).json(post)
+  }
 };
