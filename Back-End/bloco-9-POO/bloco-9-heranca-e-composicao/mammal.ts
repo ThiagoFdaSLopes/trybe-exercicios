f3df53cac69b2ba6081cfb3a131255e630c48c0c
Thiago <programadorthiagolopes@gmail.com>
2023-02-10 09:32:49 -0300

    feat: Create class mamifero

---
+++ Back-End/bloco-9-POO/bloco-9-heranca-e-composicao/mammal.ts
@@ -0,0 +1,19 @@
+import Animal from "./animalClass";
+
+class Mammal extends Animal {
+  walk() {
+    console.log(`${this.name} está andando!`);
+  }
+}
+
+const tiger = new Mammal(
+  'Tigre',
+  new Date(Date.parse('May 03, 2020')),
+);
+
+const main = (animal: Animal) => {
+  console.log(animal.age);
+}
+
+main(tiger);
+tiger.walk();
