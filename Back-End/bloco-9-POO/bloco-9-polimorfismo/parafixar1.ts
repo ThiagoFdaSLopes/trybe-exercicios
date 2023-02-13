4c8338c84e1c7f7d1f16cc4957845670db8bf6d0
Thiago <programadorthiagolopes@gmail.com>
2023-02-13 10:37:45 -0300

    feat: parafixar part 1

---
+++ Back-End/bloco-9-POO/bloco-9-polimorfismo/parafixar1.ts
@@ -0,0 +1,33 @@
+abstract class character {
+  constructor(private name: string) {}
+  abstract talk(): void
+  abstract specialMove(): void
+}
+
+class MeleeCharacter extends character {
+  constructor(private _name: string, private _specialName: string) {
+    super(_name);
+  }
+
+  talk() {
+    console.log(`O ${this._name} Disse: "Sabias Palavras"`)
+  }
+
+  specialMove() {
+    console.log(`O resultado do seu poder ${this._specialName} passou dos limites`)
+  }
+}
+
+class LongRangeCharacter extends character {
+  constructor(private _name: string, private _specialName: string) {
+    super(_name);
+  }
+
+  talk() {
+    console.log(`O ${this._name} Disse: "A distancia permite erros"`)
+  }
+
+  specialMove() {
+    console.log(`Voce causou a destruicao com seu poder ${this._specialName}`)
+  }
+}
