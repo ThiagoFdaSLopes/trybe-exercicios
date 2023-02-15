018fdda3e593da2f902decca293bf61070ffe842
Thiago <programadorthiagolopes@gmail.com>
2023-02-15 09:45:23 -0300

    feat: Iniciando dia 2 de solid

--- Back-End/bloco-9-POO/bloco-9-solid/exercicios/src/services/PlantService.ts
+++ Back-End/bloco-9-POO/bloco-9-solid/exercicios/src/services/PlantService.ts
@@ -46,22 +46,7 @@ class PlantService {
       size,
     } = plant;
 
-    if (typeof breed !== 'string') {
-      throw new HttpException(400, 'Attribute "breed" must be string.');
-    }
-
-    if (typeof needsSun !== 'boolean') {
-      throw new HttpException(400, 'Attribute "needsSun" must be boolean.');
-    }
-
-    if (typeof origin !== 'string') {
-      throw new HttpException(400, 'Attribute "origin" must be string.');
-    }
-
-    if (typeof size !== 'number') {
-      throw new HttpException(400, 'Attribute "size" must be number.');
-    }
-
+    
     const waterFrequency = needsSun
       ? size * 0.77 + (origin === 'Brazil' ? 8 : 7)
       : (size / 2) * 1.33 + (origin === 'Brazil' ? 8 : 7);
