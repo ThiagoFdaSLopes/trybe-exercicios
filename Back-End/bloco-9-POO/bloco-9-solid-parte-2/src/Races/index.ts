5841f5cf2f8c88b11111549cb75a25cc0b07c8d9
Thiago <programadorthiagolopes@gmail.com>
2023-03-07 08:25:43 -0300

    feat: Rpg Part 13

---
+++ Back-End/bloco-9-POO/bloco-9-solid-parte-2/src/Races/index.ts
@@ -0,0 +1,5 @@
+export { default as Dwarf } from './Dwarf';
+export { default as Elf } from './Elf';
+export { default as Halfling } from './Halfling';
+export { default as Orc } from './Orc';
+export { default as Race } from './Race';
