dfbc73af07c7ce4dcc99a0822571a8dfeeadc7ca
Thiago <programadorthiagolopes@gmail.com>
2023-04-26 19:18:44 -0300

    feat: Testes em Python 1

---
+++ Ciencas-Da-Computacao/bloco-1-testes/codigo.py
@@ -0,0 +1,8 @@
+def is_odd(number):
+    "Retorna True se um numero for impar, senao False"
+    return number % 2 != 0
+
+
+def divide(a_number, other_number):
+    "Retorna a divisão de a_number por other_number"
+    return a_number / other_number
